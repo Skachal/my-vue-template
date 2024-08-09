3222c79676ca128f439e5ff2dc778de8bcc16f5a
Ваше Имя <noobt061@gmail.com>
2024-08-09 19:20:35 +0600

    Removed incorrect submodule

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,8 @@
   <header class="flex justify-between border-b border-slate-300 px-10 py-5">
     <router-link to="/">
       <div class="flex gap-4 items-center">
-        <img class="w-10" :src="`${baseUrl}logo.png`" alt="Logo" />
+        <img class="w-10" src="/logo.png" alt="Logo" />
+
         <div>
           <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
           <p class="text-slate-400">Магазин лучших кроссовок</p>
